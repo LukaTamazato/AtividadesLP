b72dd06b87f4b0da0a2ec4f4a65c1e304bf4ea08
LukaTamazato <luka.tamazato@sptech.school>
2024-02-22 12:28:36 -0300

    Duas primeiras listas de exercicio adicionadas

---
+++ nivelamento-projeto/src/main/java/PrimeiraClasse.java
@@ -0,0 +1,28 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+
+        //Padrão de class = NomePadrão(pascal case)
+        //Padrão de variavel = nomeVariavel(camel case)
+
+        System.out.println("Hello World!!!");
+        // Console log
+
+        // Função == Metodos
+        // Paramêtros == Argumentos
+
+        //Tipos de Variavéis:
+        String nome = "Luka";
+        Integer numeroDaSorte = 21;
+        Double altura = 1.70;
+        Boolean bloqueado = true;
+
+        // Tipos Wrapper e Tipos primitivos
+        // Tipos Wrapper são classes, Então possuem metodos que ajudem
+        // Tipos primitivos não são classes =(
+
+        Integer numeroWraper = 42;
+        int numeroPrimitivo = 42;
+
+        System.out.println("Meu nome é " +nome+ "\nmeu numero da sorte é"+numeroDaSorte+"\nminha altura é "+altura);
+    }
+}
